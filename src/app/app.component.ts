58a36995b527445a1cc73647a84c1b0bbd03ef50
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2022-06-19 20:34:00 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'urlShorter';
+  model = {
+    inputUrl: ''
+  };
+  isFormSubmitted = false;
+  shortUrl = '';
+  isTextCopied = false;
+
+  constructor() {}
+
+    onSubmitUrlForm(urlForm: any)
+    {
+      if(urlForm.valid)
+      {
+        this.isFormSubmitted = true;
+         
+      }
+      console.log("Fomulario enviado");
+    }
+
+    copyUrl(shortUrlElementRef: any)
+    {
+      let inputElement = document.createElement('input');
+
+      inputElement.setAttribute('type', 'text');
+      inputElement.setAttribute('value', shortUrlElementRef.innetHTML);
+
+      inputElement.select();
+
+      inputElement.setSelectionRange(0, 999999); 
+
+    try
+    {
+      navigator.clipboard.writeText(inputElement.value);
+
+      this.isTextCopied = true;
+
+      setTimeout(() => {
+      this.isTextCopied = false;
+      },2000);
+
+    } catch(e) {
+      console.log('error while copying..', e.toString())
+    }
+
+      
+
+    }
+    
+    reset()
+    {
+      this.model.inputUrl = '';
+      this.isFormSubmitted = false;
+      this.isTextCopied = false;
+    }  
+  
 }
